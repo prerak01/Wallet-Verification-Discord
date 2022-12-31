36cf60c470756e8efcebec8b30fc49c9f6aee92a
Prerak <prerakmalik01@gmail.com>
2022-12-31 18:03:20 +0530

    button initial event handling

--- EventHandling/button.js
+++ EventHandling/button.js
@@ -8,11 +8,13 @@ module.exports = {
 		client.on(Events.InteractionCreate,interaction=>{
 
 			if(!interaction.isButton()) return;
-			
-
-
+				interaction.deferReply();
 
 
 		});
 	}
+}
+
+async function getWalletForm(){ // form to get wallet information from user
+	const modal 	
 }
